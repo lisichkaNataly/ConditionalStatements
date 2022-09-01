4d7a8278f31b9974c96707ce233547384bbde07d
Izmaylova <Natalyatf@yandex.ru>
2022-09-02 00:25:16 +0500

    Домашнее задание -1,2 Перезаписала задания 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        // Домашнее задание-1
-        System.out.println("Домашнее задание 1");
+        // Домашнее задание-1,2
+        System.out.println("Домашнее задание 1,2");
 
         // Задание 1
         System.out.println("Задание 1");
-        int age = 20;
+        int age = 15;
         if (age >= 18) {
             System.out.println("Поздравляем тебя с совершеннолетием!");
-        }
-        if (age < 18) {
+        } else {
             System.out.println("Возраст совершеннолетия еще не наступил, нужно подождать");
         }
 
         // Задание 2
         System.out.println("Задание 2");
-        int year = 22;
-        if (year >=7) {
+        int year = 30;
+        if (year >=7 && year < 18) {
             System.out.println("Ребенок ходит в школу");
-        }
-        if (year >=18) {
+        } else if (year >= 18 && year < 24) {
             System.out.println("Человек закончил школу, может отправляться в университет");
-        }
-        if (year >= 24) {
+        } else {
             System.out.println("Человек окончил университет и пора искать первую работу");
         }
+
         // Задание 3
         System.out.println("Задание 3");
-        short carCapacityPeople = 110;
+        short carCapacityPeople = 118;
 
 
         if (carCapacityPeople < 60) {
             System.out.println("Есть свободные сидячие места");
-        }
-        if (carCapacityPeople >= 60){
+        } else if (carCapacityPeople >= 60 && carCapacityPeople <=119){
             System.out.println("Свободных сидячих мест нет, но есть свободные стоячие места");
-        }
-        if (carCapacityPeople >= 120){
+        } else {
             System.out.println("Вагон забит полностью. Свободных мест нет");
         }
+
+
+
 
 
     }
