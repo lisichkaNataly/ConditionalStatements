ff09bdd9dbc9b2aa7856af72881a758b02bf1e12
Izmaylova <Natalyatf@yandex.ru>
2022-09-01 23:47:05 +0500

    Домашнее задание -1 Задание 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Домашнее задание-1
+        System.out.println("Домашнее задание 1");
+
+        // Задание 1
+        System.out.println("Задание 1");
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("Поздравляем тебя с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия еще не наступил, нужно подождать");
+        }
+
+        // Задание 2
+        System.out.println("Задание 2");
+        int year = 22;
+        if (year >=7) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (year >=18) {
+            System.out.println("Человек закончил школу, может отправляться в университет");
+        }
+        if (year >= 24) {
+            System.out.println("Человек окончил университет и пора искать первую работу");
+        }
+        // Задание 3
+        System.out.println("Задание 3");
+        short carCapacityPeople = 110;
+
+
+        if (carCapacityPeople < 60) {
+            System.out.println("Есть свободные сидячие места");
+        }
+        if (carCapacityPeople >= 60){
+            System.out.println("Свободных сидячих мест нет, но есть свободные стоячие места");
+        }
+        if (carCapacityPeople >= 120){
+            System.out.println("Вагон забит полностью. Свободных мест нет");
+        }
+
+
     }
 }
